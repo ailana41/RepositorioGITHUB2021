186489fa0d5442eccc02cfdf8ef86e149b62e234
ailana41 <cra.analiaturro@gmail.com>
2021-06-17 22:39:48 -0300

    Primer Commit
    este es el primer commit del proyecto

---
+++ script2.js
@@ -0,0 +1,31 @@
+/*var edad1 = prompt ('Por favor ingrese la edad de la primera persona');
+var edad2 = prompt ('Por favor ingrese la edad de la segunda persona');
+var edad3 = prompt ('Por favor ingrese la edad de la tercera persona');
+if (edad1 == edad2 || edad1 == edad3 || edad2==edad3){
+    alert("Hay dos usuarios o màs con la misma edad");
+    }
+    else{
+       if (edad1 > edad2){
+          if (edad1 > edad3){
+             alert("El Usuario 1 es Mayor y tiene "+edad1+" años");
+             }
+             else{
+                alert("El Usuario 3 es Mayor y tiene "+edad3+" años");
+                }
+       }
+       else{
+          if(edad1 < edad2){
+             if (edad2 > edad3){
+                alert("El Usuario 2 es Mayor y tiene "+edad2+" años");
+                }
+                else{
+                   alert("El Usuario 3 es Mayor y tiene "+edad3+" años");
+                   }
+             }
+          }
+    }*/
+
+var dividePorCuatro = function(number)   {
+   return  number/4;
+  }
+var division = dividePorCuatro(27);
